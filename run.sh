676ee2786c487f6996fe75f9817895def689ea9a
agilebot1 <csuhp007@gmail.com>
2024-06-08 10:40:30 +0800

    update script

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-cp examples/qwen2_lora_sft.yaml  ~/LLaMA-Factory/lora_single_gpu/
-cp data/identify.json  ~/LLaMA-Factory/data/
+cp ./examples/qwen2_lora_sft.yaml  ~/LLaMA-Factory/examples/lora_single_gpu/
+cp ./data/identify.json  ~/LLaMA-Factory/data/
