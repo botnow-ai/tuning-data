a1e84d75a9df4f7d6468c5e29f6cefefed316ff2
agilebot1 <csuhp007@gmail.com>
2024-06-08 11:20:19 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
-cp ./examples/qwen2_lora_sft.yaml  ~/LLaMA-Factory/examples/lora_single_gpu/
+cp ./examples/lora_single_gpu/qwen2_lora_sft.yaml  ~/LLaMA-Factory/examples/lora_single_gpu/
+cp ./examples/inference/qwen2_lora_sft.yaml  ~/LLaMA-Factory/examples/inference/
+cp ./examples/merge_lora/qwen2_lora_sft.yaml  ~/LLaMA-Factory/examples/merge_lora/
 cp ./data/identify.json  ~/LLaMA-Factory/data/
